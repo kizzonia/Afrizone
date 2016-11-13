84bb452fdb8d9641177152f4cc52b8df382d7894
kizzonia <chibuikeforune@hotmail.com>
2016-11-13 18:39:11 -0500

    doing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
     resources :genres do
-      resources :albumbs
+      resources :albums
     end
 
   # The priority is based upon order of creation: first created -> highest priority.
